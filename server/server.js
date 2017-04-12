eec4dd776b4779d293c366060fa81f766b8ab8ae
Kevin Schwarz <kevin@ksdesigning.info>
2017-04-11 20:43:25 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,27 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+ // GET /todos/123456
+ app.get('/todos/:id', (req, res) => {
+   var id = req.params.id;
+
+   if (!ObjectID.isValid(id)) {
+     res.status(404).send('Invalid ID');
+     return console.log('Invalid ID');
+   };
+
+  Todo.findById(id).then((todo) => {
+    if (todo) {
+      res.send({todo});
+    } else {
+      res.status(404).send('No todo');
+    }
+  }).catch((e) => {
+    res.status(400).send();
+    console.log(e);
+  });
+ });
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
